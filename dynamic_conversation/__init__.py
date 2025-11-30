ecdc9252720c5bae09db400d45964f75430b2133
Derek <derekalacy@gmail.com>
2025-11-30 11:51:35 -0800

    fixed init

--- dynamic_conversation/__init__.py
+++ dynamic_conversation/__init__.py
@@ -1,6 +1,6 @@
 """Dynamic Conversation package exports."""
 
 from .emotion_map import EmotionFlowAnalyzer
-from .load_data import EsConvData
+from .load_data import EsConv_Data
 
 __all__ = ["EmotionFlowAnalyzer", "EsConvData"]
